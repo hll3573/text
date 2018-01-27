28df646121751d9f85963f084374d2699434a960
hll3573 <915705005@qq.com>
2018-01-27 12:26:02 +0800

    test

--- src/main/java/TestThread/ThreadDemo01.java
+++ src/main/java/TestThread/ThreadDemo01.java
@@ -39,5 +39,12 @@ public class ThreadDemo01 {
         System.out.println();
         System.out.println();
         System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
